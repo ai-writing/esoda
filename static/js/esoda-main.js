61f14a77d9efece15f4d1e0b00811bd62f977a27
winedia <windy@192.168.0.110>
2017-01-23 20:22:59 +0800

    Modify the display of sidebar and "back-to-top" button.

--- static/js/esoda-main.js
+++ static/js/esoda-main.js
@@ -234,7 +234,7 @@ $(function () {
   var loadCount = 1, loading = 0, loaded = 0;
 
   $( window ).scroll( function () {
-    if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+    if ($(document).height() <= $(window).scrollTop() + $(window).height() + 20) {
       if (loading == 1 || loaded == 1 || loadCount > 4) return;
 
       loading = 1;
@@ -262,7 +262,23 @@ $(function () {
     return false;
   });
 
-  $(".affix").affix();
+  $("#SidebarAffix").affix({
+  	offset: {
+	    top: 20,
+	    bottom: function () {
+	      return (this.bottom = $(".footer").outerHeight(true) + $("#BackToTopAffix").outerHeight(true) + 40)
+	    }
+	  }
+  });
+
+  $("#BackToTopAffix").affix({
+  	offset: {
+  		top: 0,
+	    bottom: function () {
+	      return (this.bottom = $(".footer").outerHeight(true) + 20)
+	    }
+	  }
+  });
 
   $.clickHeart = function(e) {
 		e.preventDefault();
