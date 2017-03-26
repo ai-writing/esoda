c9f13fba3212520ee841a5a7edc4222085d51464
winedia <winediawu@Winedias-MacBook.local>
2017-03-26 23:04:28 +0800

    Modifications on Modals.

--- static/js/esoda-main.js
+++ static/js/esoda-main.js
@@ -328,11 +328,6 @@ $(function () {
     $(e.target).toggleClass("glyphicon-chevron-down");
   }
 
-  $.clickRemove = function(e) {
-    e.preventDefault();
-    $(e.target).parent().parent().hide();
-  }
-
   $("#OpenRegister").click(function() {
     $("#ModalLogin").modal("hide");
     setTimeout(function() {
