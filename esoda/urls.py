e8ac316ab0d018dadedb959e793521bf806eea49
winedia <windy@windydeMacBook-Pro.local>
2017-04-06 23:28:52 +0800

    Add views for index template.

--- esoda/urls.py
+++ esoda/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
+import views
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='esoda/index.html'), name='index'),
-    url(r'^result/$', TemplateView.as_view(template_name='esoda/result.html'), name='result'),
+    #url(r'^$', TemplateView.as_view(template_name='esoda/index.html'), name='index'),
+    #url(r'^result/$', TemplateView.as_view(template_name='esoda/result.html'), name='result'),
+    url(r'^$', views.home_view, name='index'),
+    url(r'^result/$', views.result_view, name='result'),
 ]
--- esoda/views.py
+++ esoda/views.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+	info = {
+		'feedbackList': [
+			{
+				'content': u'无产阶级政党的党章是以马克思主义党的学说为指导，结合党的建设的实践而制定的党的生活准则和行为规范。',
+				'user_name': u'潘星宇'
+			},
+			{
+				'content': u'无产阶级政党的党章是以马克思主义党的学说为指导，结合党的建设的实践而制定的党的生活准则和行为规范。',
+				'user_name': u'潘星宇'
+			}
+		],
+		'count_of_favorite': 12049
+	}
+	return render(request, 'esoda/index.html', info);
+
+def result_view(request):
+	return render(request, 'esoda/result.html', {});
