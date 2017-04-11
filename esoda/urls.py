c9752d6f0acbe6fdd986fbde1e9d1c277f5268d1
ZYShin <yeshuangzhu@gmail.com>
2017-04-11 22:49:47 +0800

    Login view added

--- esoda/urls.py
+++ esoda/urls.py
@@ -5,9 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    #url(r'^$', TemplateView.as_view(template_name='esoda/index.html'), name='index'),
-    #url(r'^result/$', TemplateView.as_view(template_name='esoda/result.html'), name='result'),
-    url(r'^$', views.home_view, name='index'),
+    url(r'^$', views.esoda_view, name='esoda'),
     url(r'^result/$', views.result_view, name='result'),
     url(r'^suggest/$', views.dict_suggest_view, name='dict_suggest'),
+    url(r'^sentences/$', views.sentence_view, name='sentences'),
 ]
