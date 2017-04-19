5ccf1c59d7dedb55eadcc36ea66073f18930b0f5
winedia <windy@windydeMacBook-Pro.local>
2017-04-19 13:08:47 +0800

    Add registration and activation pages.

--- esodawebsite/settings.py
+++ esodawebsite/settings.py
@@ -155,3 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
