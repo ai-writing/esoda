16a4b4c4d0d19e7c0fc5bf2bbdea8e6469c05660
winedia <windy@windydeMacBook-Pro.local>
2017-05-22 11:41:29 +0800

    Fix minor bug.

--- esoda/utils.py
+++ esoda/utils.py
@@ -57,7 +57,7 @@ def notstar(p, q):
 
 
 def gen_source_url(p):
-    year = p.get('year')
+    year = str(p.get('year'))
     title = p.get('title', '')
     authList = p.get('authors', '').split(';')
     conference = p.get('venue', '/').split('/')[-1]
