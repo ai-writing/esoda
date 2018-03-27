1c59a00cfa4e503cafc15f6a74f4c03d3da7e55b
gmx <thss15_gaomx@163.com>
2018-03-27 19:54:30 +0800

    left sidebar; feedback box

--- common/urls.py
+++ common/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'common'
 urlpatterns = [
     url(r'^comment/$', views.comment_view, name='comment'),
+    url(r'^feedback/$', views.feedback_view, name='feedback'),
 ]
