4cbeae1a02c4f8895bddf181e38b80edbd30a513
sheeta13 <mengqi_13@163.com>
2018-03-29 14:00:03 +0800

    Add flag to distinguish between syn and usage word

--- esoda/views.py
+++ esoda/views.py
@@ -146,8 +146,9 @@ def get_synonyms_dict(t, ref, i, dt, dbs, cids):
                 cnt = EsAdaptor.count(lemma.split(' '), [], dbs, cids)['hits']['total']
             else:
                 d = [{'dt': dt, 'l1': lemma.split(' ')[0], 'l2': lemma.split(' ')[1]}]
-                cnt = EsAdaptor.count([], d, dbs, cids)['hits']['total']        
-            syn_dict[tt].append({'ref': reff, 'lemma': lemma, 'content': syn, 'count': cnt})
+                cnt = EsAdaptor.count([], d, dbs, cids)['hits']['total']
+            if cnt:
+                syn_dict[tt].append({'ref': reff, 'lemma': lemma, 'content': syn, 'count': cnt, 'type': 1}) # type 1 for synonyms_word
     return syn_dict
 
 
@@ -173,7 +174,7 @@ def syn_usageList_view(request):
         ttcnt = EsAdaptor.count(t, [], dbs, cids)['hits']['total']
 
     t_list, star = star2collocation(t, dt)
-    # t_str = ' '.join(t)
+    t_str = ' '.join(t)
     info = {
         't_list': t_list,
         'count': ttcnt,
@@ -189,9 +190,9 @@ def syn_usageList_view(request):
 
     if '*' in t:
         syn_usage_dict[star] = syn_usage_dict['*']
-        # t_str = t_str.strip('*')
+        t_str = t_str.strip('*')
 
-    # info['t_str'] = t_str
+    info['t_str'] = t_str
     info['syn_usage_dict'] = syn_usage_dict
     return render(request, 'esoda/collocation_result.html', info)
 
@@ -289,7 +290,8 @@ def get_usage_dict(t, ref, i, dt, dbs, cids):
                         'ref': ' '.join(nref),
                         'lemma': pat % (l1, l2), # for query
                         'content': con, # for display
-                        'count': j['doc_count']
+                        'count': j['doc_count'],
+                        'type': 2 # for usageword
                     })
             if k[0] == '*':
                 usageDict[t[i]] = ret
@@ -323,7 +325,8 @@ def get_collocations(clist, qt, ref, i, dbs, cids):
         clist.append({
             'type': pat % (qt[i], ALL_DEPS[j % 4], qt[i + 1]),
             'label': 'Colloc%d_%d' % (len(clist), j % 4 + 1),
-            't_dt' : (list(qt), str(j % 4 + 1)), # convert_type2title(pat % (displayed_lemma(ref[i], qt[i]), ALL_DEPS[j % 4], displayed_lemma(ref[i+1], qt[i+1]))),
+            't_dt' : (list(qt), str(j % 4 + 1)),
+            'title': convert_type2title(pat % (displayed_lemma(ref[i], qt[i]), ALL_DEPS[j % 4], displayed_lemma(ref[i+1], qt[i+1]))), # todo delete
             'count' : cnt
             # 'usageList': [],
         })
@@ -332,7 +335,7 @@ def get_collocations(clist, qt, ref, i, dbs, cids):
 def collocation_list(t, ref, dbs, cids):
     # return collocation_list, default_collocation index
     cnt = EsAdaptor.count(t, [], dbs, cids)['hits']['total']
-    head = [{'count': cnt, 't_dt': (t, '0'), 'type': ' '.join(t), 'label':  'Colloc0_0'}] # all results
+    head = [{'count': cnt, 't_dt': (t, '0'), 'type': ' '.join(t), 'label':  'Colloc0_0', 'title': u'全部结果'}] # all results
     clist = []
     if len(t) >= 3:
         return head, 1
