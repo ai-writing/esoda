f5961f3d1ae1d484ab0019183fab75b5a7f42d32
sheeta13 <mengqi_13@163.com>
2018-04-20 11:17:38 +0800

    Refactor .single-extend click()

--- esoda/static/js/esoda-result.js
+++ esoda/static/js/esoda-result.js
@@ -114,10 +114,10 @@ $(function () {
         mouseWheel: {preventDefault: true},
         autoHideScrollbar: true
       });
-      if (type0.indexOf('*') > -1) { $(id + " .single-extend:eq(0)").children().click(); }
+      if (type0.indexOf('*') > -1) { $(id + ' .single-extend:eq(0) .colloc-usage:eq(0)').click(); }
       else {
         if ($(id + ' .colloc-usage:eq(0)').length) { $(id + ' .colloc-usage:eq(0)').click(); }
-        else { searchAndFillSentences({t: type0, ref: ref.join(' '), i: i - 1, dt: dt, pos: POSS, expand: expand}) }
+        else { searchAndFillSentences({t: type0, ref: ref.join(' '), i: i - 1, dt: dt, pos: POSS, expand: expand}); }
       }
     });
   });
