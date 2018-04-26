70313cd2ee666adc09a1cfc2000b3e2f7a39320f
ZYShin <yeshuangzhu@gmail.com>
2018-04-26 14:01:53 +0800

    Fix feedback systems of two pages & domain select 404 bug

--- common/templatetags/get_settings.py
+++ common/templatetags/get_settings.py
@@ -1,12 +1,9 @@
-import time, logging
-
 from django import template
 from django.conf import settings
 
-logger = logging.getLogger(__name__)
 register = template.Library()
 
 
 @register.simple_tag
 def get_settings(name):
-    return getattr(settings, name, "")
+    return getattr(settings, name, '')
